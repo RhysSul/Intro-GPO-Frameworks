5e2fe87b3a90f3e42c94c147733bbc5980b0c390
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2020-02-26 16:03:32 -0500

    Project Setup Complete

--- GPRO Framework/source/GPRO-FW-3000/foo.c
+++ GPRO Framework/source/GPRO-FW-3000/foo.c
@@ -1,3 +1,4 @@
+#include "GPRO-FW-3000/foo.h"
 int foo(int bar)
 {
 	return (bar + 1);
--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -1,8 +1,10 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <stdio.h>
+#include "GPRO-FW-3000/foo.h"
 int main()
 {
 	int test = foo(9000);
-	printf("\n %d \n", test);
+	printf("\n%d\n", test);
+
 }
